03f0706134792e977c39fdaf927cb89b82da3110
RealTegra <marcomonasterolo@live.it>
2025-04-17 16:28:48 +0200

    De-aggiornamento del sito (ahhh update)

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("button2").onclick = function() {
+        window.location.href = "https://t.me/garnet_roms";
+    };
+});
